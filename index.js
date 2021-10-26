f1dd767577de91ba8f9da9f2f2e23fb436ad276c
hqjb <hqjb@hotmail.sg>
2021-10-26 23:14:25 +0800

    Added connection to mongodb

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * Import libraries
  */
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const compression = require('compression');
 const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const app = express();
 
@@ -20,17 +25,35 @@ const PORT = parseInt(process.env.PORT) || 80;
 const distDir = path.join(__dirname, "/dist/blog-frontend");
 
 /**
+ * Initialize MongoDB Client
+ */
+const mongoClient = new MongoClient(process.env.MONGO_CLIENT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/**
  * Middleware
  */
 app.use(express.static(distDir)); // Serve angular frontend
 app.use(compression()); // Compress all routes
+app.use(express.json({ limit: '50mb' })); // Limit json sent to server
 
 app.use('/api/user', userRoutes);
 app.use('/api/article', articleRoutes);
 
 /**
- * Start server on port
+ * Connect to MongoDB and Start server on port
  */
-app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+const p0 = mongoClient.connect();
+
+Promise.all([p0]).then( () => {
+
+	app.listen(PORT, () => {
+		console.info(`Application started on port ${PORT} at ${new Date()}`);
+	});
+
+})
+.catch( err => {
+    console.error(err);
 });
