1ab25bb31807f8274be2f321ad26391b913e2454
hqjb <hqjb@hotmail.sg>
2021-10-26 11:21:24 +0800

    Imported helmet and compression

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+/**
+ * Import libraries
+ */
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+
 const app = express();
 
+/**
+ * Initialize variables
+ */
 const PORT = 80;
+const distDir = path.join(__dirname, "/dist/blog-frontend");
 
-const distDir = __dirname + "/dist/blog-frontend";
-app.use(express.static(distDir));
+/**
+ * Middleware
+ */
+app.use(express.static(distDir)); // Serve angular frontend
+app.use(compression()); // Compress all routes
+app.use(helmet()); // Prevent against well known vulnerabilities
 
+/**
+ * Start server on port
+ */
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
 });
