88dd0ea532a273db6e0064b8d2389d3604f948f3
hqjb <hqjb@hotmail.sg>
2021-10-27 21:40:12 +0800

    Images are now passed with articles

--- routes/article.js
+++ routes/article.js
@@ -9,6 +9,7 @@ module.exports = () => {
 	 * title: string;
 	 * summary: string;
 	 * content: string;
+     * image: string;
 	 * date: Date;
 	 * category: string;
 	 * tags: Array<string>;
@@ -17,12 +18,12 @@ module.exports = () => {
      */
 
     const articles = [
-        {id: 1, title: "test", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
+        {id: 1, title: "test", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
         {id: 2, title: "test2", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["2"], username: "me" },
-        {id: 3, title: "test3", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test3", tags: ["1"], username: "me" },
+        {id: 3, title: "test3", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
         {id: 4, title: "test4", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
         {id: 5, title: "test5", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 6, title: "test6", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test4", tags: ["1"], username: "me" }
+        {id: 6, title: "test6", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" }
     ];
 
     /**
