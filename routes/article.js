d7ded0fbea80f7fe66899b94920e5b20b3dce633
hqjb <hqjb@hotmail.sg>
2021-10-27 16:52:00 +0800

    Added fake articles for testing

--- routes/article.js
+++ routes/article.js
@@ -17,7 +17,12 @@ module.exports = () => {
      */
 
     const articles = [
-        {id: 1, title: "test", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["1"], username: "me" }
+        {id: 1, title: "test", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
+        {id: 2, title: "test2", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["2"], username: "me" },
+        {id: 3, title: "test3", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test3", tags: ["1"], username: "me" },
+        {id: 4, title: "test4", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
+        {id: 5, title: "test5", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
+        {id: 6, title: "test6", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test4", tags: ["1"], username: "me" }
     ];
 
     /**
