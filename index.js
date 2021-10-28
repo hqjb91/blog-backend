931056ac11c50f578eb139db3c801a973943abec
hqjb <hqjb@hotmail.sg>
2021-10-28 10:42:20 +0800

    Add SSL certification

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /**
  * Import libraries
  */
+const fs = require('fs');
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const compression = require('compression');
 const path = require('path');
+const https = require('https');
+const http = require('http');
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
@@ -21,7 +24,8 @@ const articleRoutes = require('./routes/article');
 /**
  * Initialize variables
  */
-const PORT = parseInt(process.env.PORT) || 80;
+const HTTPS_PORT = parseInt(process.env.HTTPS_PORT) || 443;
+const HTTP_PORT = parseInt(process.env.HTTP_PORT) || 80;
 const distDir = path.join(__dirname, "/dist/blog-frontend");
 
 /**
@@ -49,9 +53,26 @@ const p0 = mongoClient.connect();
 
 Promise.all([p0]).then( () => {
 
-	app.listen(PORT, () => {
-		console.info(`Application started on port ${PORT} at ${new Date()}`);
-	});
+    http.createServer(app).listen(HTTP_PORT, () => {
+        console.log(`Application started on port ${HTTP_PORT} at ${new Date()}`)
+      })
+
+    try {
+        if (fs.existsSync('/etc/letsencrypt/live/hequanjie.com/privkey.pem')) {
+            https.createServer(
+                {
+                    key: fs.readFileSync('/etc/letsencrypt/live/hequanjie.com/privkey.pem'),
+                    cert: fs.readFileSync('/etc/letsencrypt/live/hequanjie.com/cert.pem'),
+                    ca: fs.readFileSync('/etc/letsencrypt/live/hequanjie.com/chain.pem'),
+                },
+                app
+            ).listen(HTTPS_PORT, () => {
+                console.info(`Application started on port ${HTTPS_PORT} at ${new Date()}`);
+            });
+        }
+    } catch(e) {
+        console.error(e);
+    };
 
 })
 .catch( err => {
