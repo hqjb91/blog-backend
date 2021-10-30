27e36edcd1aba4607a81c8ac5136b5897dcdc9ca
hqjb <hqjb@hotmail.sg>
2021-10-31 01:36:33 +0800

    Hardcode fake article for testing

--- routes/article.js
+++ routes/article.js
@@ -17,45 +17,10 @@ module.exports = () => {
      * }
      */
 
+    // Hardcoded article for testing html and javascript content
     const articles = [
-        {id: 1, title: "test1", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
-        {id: 2, title: "test2", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["2"], username: "me" },
-        {id: 3, title: "test3", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
-        {id: 4, title: "test4", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
-        {id: 5, title: "test5", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 6, title: "test6", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" },
-        {id: 7, title: "test7", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
-        {id: 8, title: "test8", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["2"], username: "me" },
-        {id: 9, title: "test9", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
-        {id: 10, title: "test10", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
-        {id: 11, title: "test11", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 12, title: "test12", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" },
-        {id: 13, title: "test13", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
-        {id: 14, title: "test14", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["2"], username: "me" },
-        {id: 15, title: "test15", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
-        {id: 16, title: "test16", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
-        {id: 17, title: "test17", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 18, title: "test18", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" },
-        {id: 9, title: "test9", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
-        {id: 10, title: "test10", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
-        {id: 11, title: "test11", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 12, title: "test12", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" },
-        {id: 13, title: "test13", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
-        {id: 14, title: "test14", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["2"], username: "me" },
-        {id: 15, title: "test15", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
-        {id: 16, title: "test16", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
-        {id: 17, title: "test17", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 18, title: "test18", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" },
-        {id: 9, title: "test9", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
-        {id: 10, title: "test10", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
-        {id: 11, title: "test11", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 12, title: "test12", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" },
-        {id: 13, title: "test13", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test", tags: ["1", "2", "3"], username: "me" },
-        {id: 14, title: "test14", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["2"], username: "me" },
-        {id: 15, title: "test15", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test3", tags: ["1"], username: "me" },
-        {id: 16, title: "test16", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test2", tags: ["3"], username: "me" },
-        {id: 17, title: "test17", summary: "sample summary", content: "<h1>Bla bla bla</h1>", date: new Date(), category: "test", tags: ["4"], username: "me" },
-        {id: 18, title: "test18", summary: "sample summary", content: "<h1>Bla bla bla</h1>", image: "https://material.angular.io/assets/img/examples/shiba2.jpg", date: new Date(), category: "test4", tags: ["1"], username: "me" },
+        {id: 1, title: "Mechanical Engineering - Pump Sizing", summary: "When selecting a pump either for Air-Conditioning and Mechanical Ventilation services (such as chilled water pumps) or Plumbing and Sanitary services (such as domestic water pumps), the steps are similar and relatively straightforward.", 
+        content: `<div><p>When selecting a pump either for Air-Conditioning and Mechanical Ventilation services (such as chilled water pumps) or Plumbing and Sanitary services (such as domestic water pumps), the steps are similar and relatively straightforward :</p><ol><li>Determine the flow rate required</li><li>Utilize the Steady Flow Energy Equation to derive the necessary pump head</li><li>Determine the power required</li></ol><hr>`, image: "", date: new Date(2021, 10, 31), category: "Mechanical Engineering", tags: ["Mechanical Engineering", "Fluid Dynamics", "Pump sizing"], username: "He Quanjie" },
     ];
 
     /**
@@ -86,14 +51,11 @@ module.exports = () => {
         }
 
         if ( tag != '' ) {
-            console.log(tag.toString());
             const articlesSlice = articles.filter(article => article.tags.includes(tag)).slice(parseInt(offset), parseInt(offset)+parseInt(limit));
             return res.status(200).json({success: true, articlesSlice});
         }
 
         if ( category != '' ) {
-            console.log(articles[0].category);
-            // console.log(category)
             const articlesSlice = articles.filter(article => article.category == category.toString()).slice(parseInt(offset), parseInt(offset)+parseInt(limit));
             return res.status(200).json({success: true, articlesSlice});
         }
@@ -125,7 +87,7 @@ module.exports = () => {
     router.get('/tags', async (req, res) => {  
         const tags = {};
         articles.map( article => {
-            for(tag in article.tags) {
+            for(let tag of article.tags) {
                 if (tags[tag] == null)  tags[tag] = 1
                 else tags[tag]++;
             }
